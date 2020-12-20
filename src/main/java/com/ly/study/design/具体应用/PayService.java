ab27fb1befa59186c358b3ff0391733c7708241b
ly <liuyang70@100tal.com>
2020-12-20 22:54:32 +0800

    1

--- src/main/java/com/ly/study/design/具体应用/PayService.java
+++
@@ -1,9 +0,0 @@
-package com.ly.study.design.具体应用;
-
-public interface PayService {
-
-    void pcPay();
-
-    void h5pay();
-
-}
--- src/main/java/com/ly/study/design/具体应用/Serivce/WechatServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.ly.study.design.具体应用.Serivce;
-
-public class WechatServiceImpl extends AbstractPayService {
-
-    @Override
-    public void pay() {
-        System.out.println("wechatPay");
-    }
-}
